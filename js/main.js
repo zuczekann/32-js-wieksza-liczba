5bb4e24327dad93f193523e6439db41ec3449b58
zuczekann <127730541+zuczekann@users.noreply.github.com>
2023-03-22 14:28:57 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,22 @@
+
+let num1 = 2;
+let num2 = 5;
+
+function myFunction(a, b) {
+    let myRes 
+    if (num1 > num2) {
+        myRes = num1
+    } else if (num2 > num1) {
+        myRes = num2
+    }
+    return myRes;
+}
+
+let x
+x = myFunction(num1, num2);
+console.log(x)
+
+
+
+
+
